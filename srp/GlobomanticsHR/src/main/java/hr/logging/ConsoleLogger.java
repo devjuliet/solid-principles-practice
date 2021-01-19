bd71829bcdecaaf074c2a31061588c400104331a
Julieta <juliiy98@gmail.com>
2021-01-18 17:31:08 -0700

    code refactored with Single Responsibility Principle

---
+++ srp/GlobomanticsHR/src/main/java/hr/logging/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package hr.logging;
+
+public class ConsoleLogger {
+    public void writeInfo(String msg){
+        System.out.println("Info:" + msg);
+
+    }
+
+    public void writeError(String msg, Exception e){
+        System.out.println("Info:" + msg + ";" + e);
+    }
+
+}
